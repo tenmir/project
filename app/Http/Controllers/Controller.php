d88855c3fb4d47088cc399c2c23566160cc3d7dc
Vlad Piliev <v.piliev@mcart.ru>
2024-09-17 13:15:39 +0300

    qwe

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function sendSuccess($result, $status = 200) 
+    {
+        return Response::json(
+            [
+                'success' => true,
+                'data'    => $result
+            ], 
+            $status
+        );
+    }
+
+    public function sendError($result, $status = 400) 
+    {
+        return Response::json(
+            [
+                'success' => false,
+                'data'    => $result
+            ], 
+            $status
+        );
+    }
 }
